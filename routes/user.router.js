11c2ab07aedc51f4b09a596d21234b3eba200506
Mapur2 <rupammodak1@gmail.com>
2024-04-27 11:13:05 +0530

    Added welcome msg

--- routes/user.router.js
+++ routes/user.router.js
@@ -3,12 +3,15 @@ const { registerUser, loginUser, createBusiness } = require('../controllers/user
 const {insertProduct, getProducts, getProduct, updateProduct, deleteProduct} = require("../controllers/products.controller.js")
 const verifyJWT = require("../middleware/verifyJWT.js")
 const router = express.Router()
-const upload = require("../middleware/multer.js")
+const upload = require("../middleware/multer.js");
+const ApiResponse = require('../utils/ApiResponse.js');
 
 router.route('/register').post(registerUser)
 
 router.route('/login').post(loginUser)
-
+router.route("/").get(asyncHandler( (req,res)=>{
+    return res.status(200).json(new ApiResponse(200,{},"Welcome User"))
+}))
 //secured routes
 router.route("/createbusy").post(verifyJWT,createBusiness)
 router.route("/addproduct").post(verifyJWT,upload.single('image'),insertProduct)
